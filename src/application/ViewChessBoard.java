3d0ea7bafd8a9af04c59a5a59091e96465bdccfc
myNameIsKoba <gykobayashi89@gmail.com>
2023-04-07 00:28:43 -0300

    Lançamentos de excessões

--- src/application/ViewChessBoard.java
+++ src/application/ViewChessBoard.java
@@ -12,13 +12,14 @@ public class ViewChessBoard {
 	public static void printBoard(ChessPiece[][] pieces) {
 		for(int x=0; x < pieces.length; x++) {
 			
-			System.out.print((8 - x) + " ");
+			System.out.print(" || " +(8 - x) + " ");
 			for(int y=0; y < pieces.length; y++) {
 				printPiece(pieces[x][y]);
 			}
-			System.out.println("\n");
+			System.out.println("\n" + " || ");
 		}
-		System.out.println("   a   b   c   d   e   f   g   h");
+		System.out.println(" ||[=] a   b   c   d   e   f   g   h\n"
+						  +" |╞=================================");
 	}
 	
 	private static void printPiece(ChessPiece peca) {
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import boardgame.exception.BoardException;
+
 public class Board {
 
 	private Integer linhas;
@@ -7,6 +9,9 @@ public class Board {
 	private Piece[][] pecas;
 	
 	public Board(Integer linhas, Integer colunas) {
+		if (linhas < 1 || colunas < 1) {
+			throw new BoardException(" Erro ao criar o Tabuleiro: devem haver no minimo 1(uma) linha e 1(uma) coluna !");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Piece[linhas][colunas]; //deve ser instanciada informando linhas e colunas 
@@ -16,24 +21,35 @@ public class Board {
 		return linhas;
 	}
 
-	public void setLinhas(Integer linhas) {
-		this.linhas = linhas;
-	}
+//	public void setLinhas(Integer linhas) {
+//		this.linhas = linhas;
+//	}
 
 	public Integer getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(Integer colunas) {
-		this.colunas = colunas;
-	}
+//	public void setColunas(Integer colunas) {
+//		this.colunas = colunas;
+//	}
 	
-	
+	/**
+	 * sobrecarga de metodos - método que acessa uma peça em uma determindada posição
+	 * @param row
+	 * @param col
+	 * @return
+	 */
 	public Piece piece(Integer row, Integer col) {
+		if (!this.posExists(row.shortValue(), col.shortValue())) {
+			throw new BoardException("Posição inválida !");
+		}
 		return pecas[row][col];
 	}
 	
 	public Piece piece(Position pos) {
+		if (!this.posExists(pos)) {
+			throw new BoardException("Posição inválida !");
+		}
 		return pecas[pos.getRow()][pos.getCol()];
 	}
 	
@@ -43,7 +59,37 @@ public class Board {
 	 * @param pos
 	 */
 	public void placePiece(Piece peca, Position pos) {
+		if (this.thereIsAPiece(pos)) {
+			throw new BoardException("Já existe uma peça nesta posição -> " + pos);
+		}
 		this.pecas[pos.getRow()][pos.getCol()] = peca;
+		peca.pos = pos;
 	}
 	
+	/**
+	 * sobrecarga de metodos - identifica se uma posição existe
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	private Boolean posExists(Short row, Short col) {
+		return row >= 0 && row < this.linhas &&
+				col >= 0 && col < this.colunas;
+	}
+	
+	public Boolean posExists(Position pos) {
+		return posExists(pos.getRow().shortValue(), pos.getCol().shortValue());
+	}
+	
+	/**
+	 * identifica se há peças
+	 * @param pos
+	 * @return
+	 */
+	public Boolean thereIsAPiece(Position pos) {
+		if (!this.posExists(pos)) {
+			throw new BoardException("Posição inválida !");
+		}
+		return this.piece(pos) != null;
+	}
 }
